4d54c6faf0a23a2277f091bbec5a0a688bf63fa1
Neeraj Kumar <neeraj.kumar@gmail.com>
2011-06-24 14:49:28 +0530

    Second Commit

---
+++ src/com/example/android/canvas/CanvasActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.canvas;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class CanvasActivity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
