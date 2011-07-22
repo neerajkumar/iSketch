6ef2d5763ea8bf8fd853b6ce473c74be6960848d
Neeraj Kumar <neeraj.kumar@gmail.com>
2011-07-22 16:08:37 +0530

    added icons

--- src/com/example/android/canvas/CanvasActivity.java
+++ src/com/example/android/canvas/CanvasActivity.java
@@ -1,7 +1,9 @@
 package com.example.android.canvas;
 
 import android.app.Activity;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 
 public class CanvasActivity extends Activity {
     /** Called when the activity is first created. */
@@ -9,5 +11,8 @@ public class CanvasActivity extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+        View mylayout = findViewById(R.layout.main);
+        mylayout.setBackgroundColor(Color.WHITE);
+        
     }
 }
